fbd11d19e3e8ccf9e09857b03fc373da2abefd4b
YKalashnikov <uk2709@gmail.com>
2019-11-03 16:23:25 -0800

    first

---
+++ src/config.js
@@ -0,0 +1,5 @@
+const tokenUrl = "https://us1.pusherplatform.io/services/chatkit_token_provider/v1/30f274fe-2ebb-4a09-be7a-d0dac000e06f/token";
+const instanceLocator = "v1:us1:30f274fe-2ebb-4a09-be7a-d0dac000e06f";
+
+exports.tokenUrl = tokenUrl;
+exports.instanceLocator = instanceLocator;
